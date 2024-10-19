646258ee6d7fbce8a47b9ac49eb6fa4eec9b278f
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2024-10-19 08:44:37 +0600

    final added

--- index.js
+++ index.js
@@ -25,9 +25,70 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    const doctorCollection = client.db('adminPower').collection('users')
+    const doctorCollection = client.db('adminPower').collection('doctors')
+    const usersCollection = client.db('adminPower').collection('users')
 
-    app.post('/doctors',async (req,res) => {
+    // Logout
+    app.get('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+        console.log('Logout successful')
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
+
+
+    // save a user data in db
+    app.put('/user', async (req, res) => {
+      const user = req.body
+
+      const query = { email: user?.email }
+      // check if user already exists in db
+      const isExist = await usersCollection.findOne(query)
+      if (isExist) {
+        if (user.status === 'Requested') {
+          // if existing user try to change his role
+          const result = await usersCollection.updateOne(query, {
+            $set: { status: user?.status },
+          })
+          return res.send(result)
+        } else {
+          // if existing user login again
+          return res.send(isExist)
+        }
+      }
+
+      // save user for the first time
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now(),
+        },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc, options)
+      // welcome new user
+      sendEmail(user?.email, {
+        subject: 'Welcome to admin power!',
+        message: `Hope you will find you destination`,
+      })
+      res.send(result)
+    })
+
+    // Get all doctors from db
+    app.get('/doctors', async (req, res) => {
+      const result = await doctorCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post('/doctors', async (req, res) => {
       const doctorData = req.body
       const result = await doctorCollection.insertOne(doctorData)
       res.send(result)
